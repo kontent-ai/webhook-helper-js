9a73d676d1e9f29c455abbd27bc870781ebe576e
Ivan Kiral <kiralivan@gmail.com>
2025-11-06 10:33:31 +0100

    get rid of barrel files

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,27 @@
 // Public API
-export * from "./models/index.js";
-export * from "./responses/index.js";
-export * from "./signatures/index.js";
+export type {
+  AssetEvents,
+  AssetMesage,
+  ContentItemPreviewEvents,
+  ContentItemPreviewMessage,
+  ContentItemPublishedEvents,
+  ContentItemPublishedMessage,
+  ContentItemWorkflowChangedPreviewMessage,
+  ContentTypeEvents,
+  ContentTypeMessage,
+  LanguageEvents,
+  LanguageMessage,
+  TaxonomyEvents,
+  TaxonomyMessage,
+  WebhookDeliverySlot,
+  WebhookItemNotification,
+  WebhookItemObjectData,
+  WebhookNotification,
+  WebhookObjectData,
+  WebhookObjectNotification,
+} from "./models/webhook-models.js";
+
+export type { WebhookResponse } from "./responses/responses.js";
+
+export type { ISignatureHelper } from "./signatures/signature-helper.class.js";
+export { SignatureHelper, signatureHelper } from "./signatures/signature-helper.class.js";
